e8b460d1a38c9db17e6df3d5f962b03dfa41a909
ggroshansii <garthgroshansii@gmail.com>
2021-11-24 19:40:57 -0500

    added all necessary data from API to modal (book cover img, title, description and an add book to library btn); discovered modal is not updating with new search

--- script.js
+++ script.js
@@ -4,11 +4,16 @@ const titleInput = document.querySelector("#book-title");
 const authorInput = document.querySelector("#book-author");
 const subjectInput = document.querySelector("#book-subject");
 
+const modal = new bootstrap.Modal(document.getElementById('bookModal'));
+const modalBody = document.querySelector('.modal-body');
+
 let searchData = {
     title: "",
     author: "",
     subject: "",
 }
+
+let searchResults;
 
 searchBtn.addEventListener("click", (e) => {
     e.preventDefault()
@@ -18,8 +23,14 @@ searchBtn.addEventListener("click", (e) => {
         subject: subjectInput.value,
 
     }
-    console.log(searchData)
-    queryBooks()
+    modal.toggle()
+
+    queryBooks();
+
+    setTimeout(() => {
+        displaySearchResults();
+
+    }, 900)
 })
 
 async function queryBooks() {
@@ -28,7 +39,53 @@ async function queryBooks() {
     }
     const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=intitle:${searchData.title}&inauthor:${searchData.author}&insubject:${searchData.subject}&key=AIzaSyDhnFHiBiax8maT3xgRGpe14SUPQG8iaMc`, options);
     const data = await response.json();
-    console.log(data)
+    searchResults = data;
 }
 
+
+function displaySearchResults() {
+    searchResults.items.forEach(item => {
+        
+        if (item.volumeInfo.imageLinks) {
+            let bookOuterContainer = document.createElement('div');
+            bookOuterContainer.classList.add("modal-book-outer-container")
+
+            let bookTitle = document.createElement('h2');
+            bookTitle.textContent = item.volumeInfo.title;
+            bookOuterContainer.append(bookTitle);
+
+            let bookInnerContainer = document.createElement('div');
+            bookInnerContainer.classList.add("modal-book-inner-container");
+
+            let bookImgDiv = document.createElement('div');
+            let bookImg = document.createElement('img');
+            bookImg.src = item.volumeInfo.imageLinks.thumbnail;
+            bookImgDiv.append(bookImg);
+            bookInnerContainer.append(bookImgDiv);
+
+            let bookDescBtnContainer = document.createElement('div');
+            bookDescBtnContainer.classList.add('modal-book-desc-btn-container')
+            let bookDescDiv = document.createElement('div');
+            let bookDescription = document.createElement('p');
+            bookDescription.textContent = item.volumeInfo.description;
+            bookDescDiv.append(bookDescription);
+            bookDescBtnContainer.append(bookDescDiv);
+            let bookBtnDiv = document.createElement('div');
+            let bookAddBtn = document.createElement('button');
+            bookAddBtn.textContent = "Add Book to Library";
+            bookAddBtn.classList.add("btn", "btn-success", "modal-book-add-btn")
+            bookBtnDiv.append(bookAddBtn);
+            bookDescBtnContainer.append(bookBtnDiv);
+
+            bookInnerContainer.append(bookDescBtnContainer);
+            bookOuterContainer.append(bookInnerContainer);
+
+            modalBody.append(bookOuterContainer);
+        }
+
+
+        console.log(item.volumeInfo)
+    })
+    modal.show();
+}
 
